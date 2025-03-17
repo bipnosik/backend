78aa8ab2bd9e31851a3c4a4ac4a91c4c04d92388
zenfore1 <naxim2006@gmail.com>
2025-03-18 00:22:44 +0300

    одключение PostgreSQL через приватную сетsыь

--- SiteC/settings.py
+++ SiteC/settings.py
@@ -103,11 +103,13 @@ WSGI_APPLICATION = 'SiteC.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-
-if DATABASE_URL:
+if 'RAILWAY_ENVIRONMENT' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+        'default': dj_database_url.parse(
+            'postgresql://postgres:HgUHSxhNNtbZzEQvQfkjxNStTaGFHOsw@postgres.railway.internal:5432/railway',
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
 else:
     DATABASES = {
